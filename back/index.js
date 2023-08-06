c82bae0004e3424ea63f000f24d8d2e30c91b13b
Shaulaev Musa <90182906+shaulaev@users.noreply.github.com>
2023-08-07 02:28:00 +0300

    Updated back

--- back/index.js
+++ back/index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors(
     {
-        origin: ["https://true-blog-v2.vercel.app"],
+        origin: ["https://true-blog-v2.vercel.app/*"],
         methods: ["GET", "POST", "DELETE", "PUT", "PATCH", "OPTION"],
         credentials: true
     }
