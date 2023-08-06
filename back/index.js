1be7802988550a3dd3658a343e4e3cc2f622d51b
Molodec <molodec@example.com>
2023-08-07 02:43:10 +0300

    GPT

--- back/index.js
+++ back/index.js
@@ -1,31 +1,50 @@
-require('dotenv').config()
-const express = require("express")
-const mongoose = require("mongoose")
-const cors = require("cors")
-const cookieParser = require("cookie-parser")
+require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
-const app = express()
-const PORT = process.env.PORT || 5000
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
+
+// Логирование перед middleware CORS
+app.use((req, res, next) => {
+    console.log("Before CORS middleware");
+    next();
+});
+
+// Убедитесь, что middleware CORS стоит перед маршрутами
 app.use(cors(
     {
-        origin: ["https://true-blog-v2.vercel.app/*"],
+        origin: ["https://true-blog-v2.vercel.app"],
         methods: ["GET", "POST", "DELETE", "PUT", "PATCH", "OPTION"],
         credentials: true
     }
-))
-app.use(require("./routes/Users.route.js"))
-app.use(require("./routes/Posts.route.js"))
-app.use("/uploads", express.static(__dirname + "/uploads"))
+));
+
+// Логирование после middleware CORS
+app.use((req, res, next) => {
+    console.log("After CORS middleware");
+    next();
+});
+
+app.use(require("./routes/Users.route.js"));
+app.use(require("./routes/Posts.route.js"));
+app.use("/uploads", express.static(__dirname + "/uploads"));
 
 const start = async () => {
-    await mongoose.connect("mongodb+srv://trueBlog:nicePasswordOK@cluster0.xnj3lfh.mongodb.net/?retryWrites=true&w=majority")
-    console.log("подключился к MongoDB")
-    app.listen(PORT, () => {
-        console.log(`Сервер успешно запущен на порте ${PORT}`)
-    })
-}
+    try {
+        await mongoose.connect("mongodb+srv://trueBlog:nicePasswordOK@cluster0.xnj3lfh.mongodb.net/?retryWrites=true&w=majority");
+        console.log("Подключился к MongoDB");
+        app.listen(PORT, () => {
+            console.log(`Сервер успешно запущен на порте ${PORT}`);
+        });
+    } catch (error) {
+        console.error("Ошибка при подключении к MongoDB:", error);
+    }
+};
 
-start()
+start();
