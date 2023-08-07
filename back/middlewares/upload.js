4b54a7028122bf5dc122cf4eb79590ee7ac5a59e
Molodec <molodec@example.com>
2023-08-07 03:58:39 +0300

    Upload

--- back/middlewares/upload.js
+++ back/middlewares/upload.js
@@ -3,7 +3,7 @@ const multer = require('multer')
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/')
+    cb(null, __dirname + '/uploads/')
   },
   filename: function (req, file, cb) {
     let ext = path.extname(file.originalname)
